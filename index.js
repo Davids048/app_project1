3387c888e3aa3219ab3ffd691088049bdd30a7cf
Davids048 <90978028+Davids048@users.noreply.github.com>
2022-08-31 01:18:37 -0500

    grouped resolvers and typeDefs in graphql, still working fine

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
 const {ApolloServer} = require ('apollo-server');
-const gql = require ('graphql-tag');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
-
+const typeDefs = require('./graphgql/typeDefs');
+const resolvers = require('./graphgql/resolvers');
 const {MONGODB} = require('./config.js'); // putting in brackets deconstructs the returned stuff
 
-const typeDefs = gql`
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-    type Query{
-        getPosts: [Post]
-    }
-`;
 
-const resolvers = {
-    Query:{
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            }
-            catch(err){
-                throw new Error(err);
-            }
-        }
-    }
-};
+
+
 
 
 const server = new ApolloServer({
