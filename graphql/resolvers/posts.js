9b9a568ab78b27fa35089d18ee2bfe12ccd6e534
Davids048 <90978028+Davids048@users.noreply.github.com>
2022-08-30 23:46:16 -0500

    first commit otherwise all lost

---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,15 @@
+const Post = require('../../models/Post');
+
+module.export = {
+    Query:{
+        async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            }
+            catch(err){
+                throw new Error(err);
+            }
+        }
+    }
+}
